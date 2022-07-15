98ced1ac4b478087571a225d7995700d8a82fa72
David Garcia <david_5043@hotmail.com>
2022-07-15 17:15:58 -0500

    Updates

--- src/events/types/order-status.ts
+++ src/events/types/order-status.ts
@@ -1,15 +1,17 @@
 export enum OrderStatus {
-  //  when the order has been created, but the ticket it is trying to order has not been reserved
+  // When the order has been created, but the
+  // ticket it is trying to order has not been reserved
   Created = 'created',
 
-  // The ticket and/or order are trying to be reserved has already been reserved
-  // or when the user cancelled the order
-  // or the order expires before payment
+  // The ticket the order is trying to reserve has already
+  // been reserved, or when the user has cancelled the order.
+  // The order expires before payment
   Cancelled = 'cancelled',
 
   // The order has successfully reserved the ticket
   AwaitingPayment = 'awaiting:payment',
 
-  // The order has reserved the ticket and the user has provided payment successfully
+  // The order has reserved the ticket and the user has
+  // provided payment successfully
   Complete = 'complete',
 }
